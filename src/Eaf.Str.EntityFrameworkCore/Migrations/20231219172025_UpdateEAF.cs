cd5621e60596bd7c30db968233e6adb863076930
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2023-12-19 14:23:18 -0300

    migration EAF

---
+++ src/Eaf.Str.EntityFrameworkCore/Migrations/20231219172025_UpdateEAF.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eaf.Str.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEAF : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "EafPermissions",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "EafFeatures",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "EafTenantAddress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ZipCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    Neighborhood = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Complement = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Observation = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: true),
+                    Document = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    TenantId = table.Column<int>(type: "int", nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EafTenantAddress", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EafTenantAddress_EafTenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "EafTenants",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EafTenantAddress_TenantId",
+                table: "EafTenantAddress",
+                column: "TenantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EafTenantAddress");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "EafPermissions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "EafFeatures",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
