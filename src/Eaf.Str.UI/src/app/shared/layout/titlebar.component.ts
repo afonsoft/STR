591d5de5e217442eacf80521fb62ac7212280c33
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2023-12-19 15:26:27 -0300

    tiitlebar

--- src/Eaf.Str.UI/src/app/shared/layout/titlebar.component.ts
+++ src/Eaf.Str.UI/src/app/shared/layout/titlebar.component.ts
@@ -1,11 +1,11 @@
 import { Component, Injector } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 
-import * as _ from 'lodash';
-
 @Component({
   selector: 'titlebar',
-  template: `<h3 class="m_header_tilebar header-{{ currentTheme.baseSettings.header.headerSkin }}">Str</h3>`,
+  template: `<h3 class="m_header_tilebar header-{{ currentTheme.baseSettings.header.headerSkin }}">
+    Sistema de Transporte e Rastreamento
+  </h3>`,
 })
 export class TitleBarComponent extends AppComponentBase {
   constructor(injector: Injector) {
