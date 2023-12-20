d1de6ef5fed0e7786c29d7cebc6a3b3125d4d632
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2023-12-20 08:58:41 -0300

    airports job

--- src/Eaf.Str.UI/src/shared/service-proxies/service-proxies.ts
+++ src/Eaf.Str.UI/src/shared/service-proxies/service-proxies.ts
@@ -1271,6 +1271,53 @@ export class AirportsServiceProxy {
         }
         return _observableOf<AirportDto[]>(null as any);
     }
+
+    /**
+     * @return Success
+     */
+    startJob(): Observable<void> {
+        let url_ = this.baseUrl + "/api/services/app/Airports/StartJob";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processStartJob(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processStartJob(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<void>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<void>;
+        }));
+    }
+
+    protected processStartJob(response: HttpResponseBase): Observable<void> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return _observableOf<void>(null as any);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<void>(null as any);
+    }
 }
 
 @Injectable()
