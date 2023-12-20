cf020f3d2ff4f924b64aa0741cb5b7e1e8d2b29e
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2023-12-20 12:07:53 -0300

    CEP

--- src/Eaf.Str.UI/src/app/main/cep/cep.component.ts
+++ src/Eaf.Str.UI/src/app/main/cep/cep.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { ViaCepAddressDto, ViaCepServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-cep',
@@ -10,8 +11,29 @@ import { AppComponentBase } from '@shared/common/app-component-base';
   animations: [appModuleAnimation()],
 })
 export class CepComponent extends AppComponentBase implements OnInit {
-  constructor(injector: Injector) {
+  filters: {
+    filterText: string;
+  } = <any>{};
+
+  public viaCepAddressDto: ViaCepAddressDto = <ViaCepAddressDto>{};
+  public mask: Array<string | RegExp>;
+
+  constructor(
+    injector: Injector,
+    private _viaCepServiceProxy: ViaCepServiceProxy,
+  ) {
     super(injector);
+    this.mask = [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/];
+    this.viaCepAddressDto = new ViaCepAddressDto();
   }
   ngOnInit(): void {}
+
+  getCep() {
+    this.dataTableHelper.showLoadingIndicator();
+    this.viaCepAddressDto = new ViaCepAddressDto();
+    this._viaCepServiceProxy.getAddress(this.filters.filterText).subscribe(result => {
+      this.viaCepAddressDto = result;
+      this.dataTableHelper.hideLoadingIndicator();
+    });
+  }
 }
