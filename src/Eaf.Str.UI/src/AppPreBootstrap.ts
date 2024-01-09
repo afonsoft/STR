7455e8abfd1d7b724e71b05ccb23d0d60ebb3626
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2024-01-09 15:13:21 -0300

    ajustes do pre bootstrap

--- src/Eaf.Str.UI/src/AppPreBootstrap.ts
+++ src/Eaf.Str.UI/src/AppPreBootstrap.ts
@@ -1,152 +1,159 @@
-import { StorageService } from '@eaf/utils/storage.service';
 import { CompilerOptions, NgModuleRef, Type } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { StorageService } from '@eaf/utils/storage.service';
 import { AppConsts } from '@shared/AppConsts';
-import { UrlHelper } from './shared/helpers/UrlHelper';
-import { XmlHttpRequestHelper } from '@shared/helpers/XmlHttpRequestHelper';
 import { DynamicResourcesHelper } from '@shared/helpers/DynamicResourcesHelper';
-import { environment } from './environments/environment';
-
-
-import * as moment from 'moment';
+import { XmlHttpRequestHelper } from '@shared/helpers/XmlHttpRequestHelper';
 import * as _ from 'lodash';
+import * as moment from 'moment';
+
+import { environment } from './environments/environment';
+import { UrlHelper } from './shared/helpers/UrlHelper';
 
 export class AppPreBootstrap {
+  private static storageService: StorageService;
 
-    private static storageService: StorageService;
+  static Init(storageService: StorageService) {
+    this.storageService = storageService;
+  }
 
-    static Init(storageService: StorageService) {
-        this.storageService = storageService;
-    }
+  static run(appRootUrl: string, callback: () => void, resolve: any, reject: any): void {
+    AppPreBootstrap.getApplicationConfig(appRootUrl, () => {
+      const queryStringObj = UrlHelper.getQueryParameters();
 
-    static run(appRootUrl: string, callback: () => void, resolve: any, reject: any): void {
-        AppPreBootstrap.getApplicationConfig(appRootUrl, () => {
-
-            const queryStringObj = UrlHelper.getQueryParameters();
-
-            if (queryStringObj.impersonationToken) {
-                AppPreBootstrap.impersonatedAuthenticate(queryStringObj.impersonationToken, queryStringObj.tenantId, () => { AppPreBootstrap.getUserConfiguration(callback); });
-            } else {
-                AppPreBootstrap.getUserConfiguration(callback);
-            }
+      if (queryStringObj.impersonationToken) {
+        AppPreBootstrap.impersonatedAuthenticate(queryStringObj.impersonationToken, queryStringObj.tenantId, () => {
+          AppPreBootstrap.getUserConfiguration(callback);
         });
+      } else {
+        AppPreBootstrap.getUserConfiguration(callback);
+      }
+    });
+  }
+
+  static bootstrap<TM>(moduleType: Type<TM>, compilerOptions?: CompilerOptions | CompilerOptions[]): Promise<NgModuleRef<TM>> {
+    return platformBrowserDynamic().bootstrapModule(moduleType, compilerOptions);
+  }
+
+  private static getApplicationConfig(appRootUrl: string, callback: () => void) {
+    let type = 'GET';
+    let url = appRootUrl + 'assets/' + environment.appConfig;
+    let customHeaders = [
+      {
+        name: 'Eaf.TenantId',
+        value: eaf.multiTenancy.getTenantIdCookie() + '',
+      },
+    ];
+
+    XmlHttpRequestHelper.ajax(type, url, customHeaders, null, result => {
+      AppConsts.appBaseUrlFormat = result.appBaseUrl;
+      AppConsts.remoteServiceBaseUrlFormat = result.remoteServiceBaseUrl;
+      AppConsts.localeMappings = result.localeMappings;
+
+      AppConsts.appBaseUrl = result.appBaseUrl;
+      AppConsts.remoteServiceBaseUrl = result.remoteServiceBaseUrl;
+
+      callback();
+    });
+  }
+
+  private static getCurrentClockProvider(currentProviderName: string): eaf.timing.IClockProvider {
+    if (currentProviderName === 'unspecifiedClockProvider') {
+      return eaf.timing.unspecifiedClockProvider;
     }
 
-    static bootstrap<TM>(moduleType: Type<TM>, compilerOptions?: CompilerOptions | CompilerOptions[]): Promise<NgModuleRef<TM>> {
-        return platformBrowserDynamic().bootstrapModule(moduleType, compilerOptions);
+    if (currentProviderName === 'utcClockProvider') {
+      return eaf.timing.utcClockProvider;
     }
 
-    private static getApplicationConfig(appRootUrl: string, callback: () => void) {
-        let type = 'GET';
-        let url = appRootUrl + 'assets/' + environment.appConfig;
-        let customHeaders = [
-            {
-                name: 'Eaf.TenantId',
-                value: eaf.multiTenancy.getTenantIdCookie() + ''
-            }];
+    return eaf.timing.localClockProvider;
+  }
 
-        XmlHttpRequestHelper.ajax(type, url, customHeaders, null, (result) => {
+  private static impersonatedAuthenticate(impersonationToken: string, tenantId: number, callback: () => void): void {
+    eaf.multiTenancy.setTenantIdCookie(tenantId);
+    const cookieLangValue = this.storageService.getCookieValue('Eaf.Localization.CultureName');
 
-            AppConsts.appBaseUrlFormat = result.appBaseUrl;
-            AppConsts.remoteServiceBaseUrlFormat = result.remoteServiceBaseUrl;
-            AppConsts.localeMappings = result.localeMappings;
+    let requestHeaders = {
+      '.AspNetCore.Culture': 'c=' + cookieLangValue + '|uic=' + cookieLangValue,
+      'Eaf.TenantId': eaf.multiTenancy.getTenantIdCookie(),
+      'Eaf.Localization.CultureName': cookieLangValue,
+      'Accept-Language': cookieLangValue,
+    };
 
-            AppConsts.appBaseUrl = result.appBaseUrl;
-            AppConsts.remoteServiceBaseUrl = result.remoteServiceBaseUrl;
+    XmlHttpRequestHelper.ajax(
+      'POST',
+      AppConsts.remoteServiceBaseUrl + '/api/TokenAuth/ImpersonatedAuthenticate?impersonationToken=' + impersonationToken,
+      requestHeaders,
+      null,
+      response => {
+        let result = response.result;
+        eaf.auth.setToken(result.accessToken);
+        AppPreBootstrap.setEncryptedTokenCookie(result.encryptedAccessToken);
+        location.search = '';
+        callback();
+      },
+    );
+  }
 
-            callback();
-        });
+  private static getUserConfiguration(callback: () => void): any {
+    var cookieLangValue = this.storageService.getCookieValue('Eaf.Localization.CultureName');
+
+    if (cookieLangValue == null || cookieLangValue == '') {
+      cookieLangValue = 'pt-BR';
+      this.storageService.setCookieValue(
+        'Eaf.Localization.CultureName',
+        cookieLangValue,
+        new Date(new Date().getTime() + 5 * 365 * 86400000), // 5 year
+        eaf.appPath,
+      );
     }
 
-    private static getCurrentClockProvider(currentProviderName: string): eaf.timing.IClockProvider {
-        if (currentProviderName === 'unspecifiedClockProvider') {
-            return eaf.timing.unspecifiedClockProvider;
+    const token = this.storageService.getCookieValue(eaf.auth.tokenCookieName);
+
+    let requestHeaders = {
+      '.AspNetCore.Culture': 'c=' + cookieLangValue + '|uic=' + cookieLangValue,
+      'Eaf.TenantId': eaf.multiTenancy.getTenantIdCookie(),
+      'Eaf.Localization.CultureName': cookieLangValue,
+      'Accept-Language': cookieLangValue,
+    };
+
+    if (token) {
+      requestHeaders['Authorization'] = 'Bearer ' + token;
+    }
+
+    return XmlHttpRequestHelper.ajax(
+      'GET',
+      AppConsts.remoteServiceBaseUrl + '/EafUserConfiguration/GetAll',
+      requestHeaders,
+      null,
+      response => {
+        let result = response.result;
+
+        _.merge(eaf, result);
+
+        eaf.clock.provider = this.getCurrentClockProvider(result.clock.provider);
+
+        moment.locale(eaf.localization.currentLanguage.name);
+        (window as any).moment.locale(eaf.localization.currentLanguage.name);
+
+        if (eaf.clock.provider.supportsMultipleTimezone) {
+          moment.tz.setDefault(eaf.timing.timeZoneInfo.iana.timeZoneId);
+          (window as any).moment.tz.setDefault(eaf.timing.timeZoneInfo.iana.timeZoneId);
         }
 
-        if (currentProviderName === 'utcClockProvider') {
-            return eaf.timing.utcClockProvider;
-        }
+        eaf.event.trigger('eaf.dynamicScriptsInitialized');
 
-        return eaf.timing.localClockProvider;
-    }
+        DynamicResourcesHelper.loadResources(callback);
+      },
+    );
+  }
 
-    private static impersonatedAuthenticate(impersonationToken: string, tenantId: number, callback: () => void): void {
-        eaf.multiTenancy.setTenantIdCookie(tenantId);
-        const cookieLangValue = this.storageService.getCookieValue('Eaf.Localization.CultureName');
-
-        let requestHeaders = {
-            '.AspNetCore.Culture': ('c=' + cookieLangValue + '|uic=' + cookieLangValue),
-            'Eaf.TenantId': eaf.multiTenancy.getTenantIdCookie(),
-            'Eaf.Localization.CultureName': cookieLangValue
-        };
-
-        XmlHttpRequestHelper.ajax(
-            'POST',
-            AppConsts.remoteServiceBaseUrl + '/api/TokenAuth/ImpersonatedAuthenticate?impersonationToken=' + impersonationToken,
-            requestHeaders,
-            null,
-            (response) => {
-                let result = response.result;
-                eaf.auth.setToken(result.accessToken);
-                AppPreBootstrap.setEncryptedTokenCookie(result.encryptedAccessToken);
-                location.search = '';
-                callback();
-            }
-        );
-    }
-
-
-    private static getUserConfiguration(callback: () => void): any {
-        var cookieLangValue = this.storageService.getCookieValue('Eaf.Localization.CultureName');
-
-        if (cookieLangValue == null || cookieLangValue == '') {
-            cookieLangValue = 'pt-BR';
-            this.storageService.setCookieValue(
-                'Eaf.Localization.CultureName',
-                cookieLangValue,
-                new Date(new Date().getTime() + 5 * 365 * 86400000), // 5 year
-                eaf.appPath
-            );
-        }
-
-        const token = this.storageService.getCookieValue(eaf.auth.tokenCookieName);
-
-        let requestHeaders = {
-            '.AspNetCore.Culture': ('c=' + cookieLangValue + '|uic=' + cookieLangValue),
-            'Eaf.TenantId': eaf.multiTenancy.getTenantIdCookie()
-        };
-
-        if (token) {
-            requestHeaders['Authorization'] = 'Bearer ' + token;
-        }
-
-        return XmlHttpRequestHelper.ajax('GET', AppConsts.remoteServiceBaseUrl + '/EafUserConfiguration/GetAll', requestHeaders, null, (response) => {
-            let result = response.result;
-
-            _.merge(eaf, result);
-
-            eaf.clock.provider = this.getCurrentClockProvider(result.clock.provider);
-
-            moment.locale(eaf.localization.currentLanguage.name);
-            (window as any).moment.locale(eaf.localization.currentLanguage.name);
-
-            if (eaf.clock.provider.supportsMultipleTimezone) {
-                moment.tz.setDefault(eaf.timing.timeZoneInfo.iana.timeZoneId);
-                (window as any).moment.tz.setDefault(eaf.timing.timeZoneInfo.iana.timeZoneId);
-            }
-
-            eaf.event.trigger('eaf.dynamicScriptsInitialized');
-
-            DynamicResourcesHelper.loadResources(callback);
-        });
-    }
-
-    private static setEncryptedTokenCookie(encryptedToken: string) {
-
-        this.storageService.setCookieValue(AppConsts.authorization.encrptedAuthTokenName,
-            encryptedToken,
-            new Date(new Date().getTime() + 365 * 86400000), //1 year
-            eaf.appPath
-        );
-    }
+  private static setEncryptedTokenCookie(encryptedToken: string) {
+    this.storageService.setCookieValue(
+      AppConsts.authorization.encrptedAuthTokenName,
+      encryptedToken,
+      new Date(new Date().getTime() + 365 * 86400000), //1 year
+      eaf.appPath,
+    );
+  }
 }
